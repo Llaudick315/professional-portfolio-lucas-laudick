98b90959536ec378cf36820a7f241bc8a350fedc
Lucas Lauick <llaudick@cnm.edu>
2016-04-19 12:52:59 -0600

    added .gitignore and milestone-1 infor

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>Lucas Laudick's Professional Portfolio</title>
+	</head>
+	<body>
+		<header>
+			<h1>Lucas Laudick's Web Development Portfolio</h1>
+		</header>
+		<main>
+			<h2>Purpose, Audience, Goal</h2>
+			<h3>Purpose</h3>
+			<p>This Professional Portfolio website will detail the work I have accomplished with the front-end web development
+				heading into my career.</p>
+			<h3>Audience</h3>
+			<p>My target audience will include Web development recruiters, potential employers and future clients.
+				This site is directed at these potential individuals and the professionalism they are hoping to acquire.</p>
+			<h3>Goal</h3>
+			<p>The main goal of this website will be used to help me acquire a full time job in the Web Develpment industry. The
+				website was designed to meet the eye of these potential individuals and to commence contact between myself and
+				these future employers. This website will be used as well to present myself and my ability to create a webpage
+				and the knowledge and ability I have acquired over the past couple of months. </p>
+			<h2>Persona</h2>
+			<h3>Name: Jeffery Adams</h3>
+			<h3>Age: 45</h3>
+			<h3>Profession:</h3>
+			<p>Jeffery works at a ......... his advertising company helps local businesses attract all different types of customer markets,
+				his company is always looking for new ways to market their clients. The company has had much success in creating lots of business for his
+				clients, and with all the success Jeffery is wanting to expand even more and bring in new ideas to take it to the next level. As well as
+				bring in someone to help maintain the website and mobile app to make it more user friendly and more attractive to his clients.</p>
+			<h3>Technology:</h3>
+			<p>Mac user, with Mac desktop at the office, macbook laptops and Ipads to travel with and for home use for easy link between home and work,
+				and Iphone 6 to link with desktops and Ipads.</p>
+			<h3>Attitudes and Behaviors:</h3>
+			<p>Full schedule with the rising success of the business and growing clientele, with full schedule jeffery can only look at
+				applicants every so often and is looking for the portfolio that really has a wow factor that can bring something great to his company. Uses linkedIn
+				as a good source to find local candidates and attends local events as well.</p>
+			<h3>Frustrations and Needs:</h3>
+			<p>Jeffery is most frustrated at the way his site and mobile app works and how it isn't as user friendly as he would like. His web developers now don't
+				fully understand what he is looking for in the site and mobile app. Jeffery needs to hire someone who is from a younger generation that knows the
+				quality and user friendly requirements that come with the successful businesses today. He is in need of this soon with his rapidly growing clientele
+				market. As well as a sufficient background in teamwork and team building skills, the candidate will also have great organization and people skills
+				aid in the organization already set in place.</p>
+			<h3>Goals:</h3>
+			<p>Jeffery's main goal is to bring in a new candidate to fill the Web developer position and to help with the website and application to make them
+				more user friendly and give the website new features and a more attractive look. He hopes to find the candidate with the wow factor on their
+				website that he can bring to his company and create future success with all of his clientele.</p>
+			<h2>Use Case:</h2>
+			<h3>Goal: To discover a new employee through networking and check out personal website's that are creative and has the wow factor needed for the companies
+				expansion.</h3>
+			<ol>
+				<li>Jeffery was at a local networking event where he met myself and I was able to provide him with my information including my resume and told him
+					about my linkedIn, and personal portfolio website.</li>
+				<li>While Jeffery has some time between meetings he uses his Ipad to read over my resume and check out both my linkedIn profile and personal website.</li>
+				<li>After looking over all of my skills and previous employment on LinkedIn Jeffery proceeds to click on the link to my personal website portfolio.</li>
+				<li>The website begins to load and jeffery can now explore the website.</li>
+				<li>While having a short period of time Jeffery briefly looks over the website, and looks for the key aspects that can help his company stand out.</li>
+				<li>Jeffery has looked at many websites and looks for the one that has a great mobile version as well.</li>
+				<li>While Jeffery is very busy with his schedule he only has so much time to look over portfolios and decide if it has the wow factor he is looking for.</li>
+			</ol>
+		</main>
+	</body>
+</html>
