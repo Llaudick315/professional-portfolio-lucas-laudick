0c6fd55fd1c36799c58b4911bed0b7e979238e31
Lucas Lauick <llaudick@cnm.edu>
2016-04-25 21:51:08 -0600

    added milestone-2.php and added come content

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="em">
+	<head>
+		<meta charset="utf-8"/>
+		<title>Phase II of PWP</title>
+	</head>
+	<body>
+		<header>
+			<h1>Phase II of My Professional Portfolio</h1>
+		</header>
+		<main>
+			<h2>SiteMap</h2>
+			<h3>Pages included on Website</h3>
+			<ol>
+				<li>Home Page</li>
+				<li>About me/History</li>
+				<li>My work/projects</li>
+				<li>Contact me</li>
+			</ol>
+			<h2>Content Strategy</h2>
+			<h3>Home page</h3>
+			<p>The home page will be of course the first page the user will see when looking at my website. It will have all the
+			links to the other pages across the top of the page. It will also have brief detail of what my site is about, and the
+			content you will find on the website. The home page will alos show a small slide show of my talents and what I can do
+			and what skills i have acquired. </p>
+			<h3>Interaction Flow</h3>
+		</main>
+	</body>
+</html>
