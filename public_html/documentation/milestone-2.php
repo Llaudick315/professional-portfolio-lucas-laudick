bd75eb38c0b17844f036715cce64e0a02b6b63e3
Lucas Lauick <llaudick@cnm.edu>
2016-04-29 17:45:30 -0600

    added content to milestone-2

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -13,17 +13,41 @@
 			<h3>Pages included on Website</h3>
 			<ol>
 				<li>Home Page</li>
-				<li>About me/History</li>
+				<li>About me</li>
 				<li>My work/projects</li>
 				<li>Contact me</li>
 			</ol>
 			<h2>Content Strategy</h2>
 			<h3>Home page</h3>
 			<p>The home page will be of course the first page the user will see when looking at my website. It will have all the
-			links to the other pages across the top of the page. It will also have brief detail of what my site is about, and the
-			content you will find on the website. The home page will alos show a small slide show of my talents and what I can do
-			and what skills i have acquired. </p>
-			<h3>Interaction Flow</h3>
+				links to the other pages across the top of the page. It will also have brief detail of what my site is about, and the
+				content you will find on the website. The home page will alos show a small slide show of my talents and what I can do
+				and what skills i have acquired. </p>
+			<h3>About Me</h3>
+			<p>The about me page will incude everything there is to now about me, from my education to all the work experience I have.
+				This page will also feature my personality aspects as well as my work ethics to attract the type of work enviornment that I
+				can thrive in and be very successful. The page will also include the aspirations I have and the confidence that I can bring
+				multiple attributes to the table.</p>
+			<h3>My Work/Projects</h3>
+			<p>The work and projects page will include the work I have done with software and web development. This page will mostly focus
+				on my capstone project and will be added to once I am able to get some more projects done</p>
+			<h3>Contact Me</h3>
+			<p>The contact me page will include all the possible ways to reach me. This will include my github,linkedIn account,as well as my
+				direct email. This will also include the basic format to contact me through the website, allowing the user to input ther information
+				and there contact information so I can return any responses I get through the website.</p>
+			<h2>Interaction Flow</h2>
+			<p>Anthony is looking at new candidates for the job posting his company has with his macbook at work and has decided to visit my
+				github and linkedIn profile, and is interested in looking at the personal website I have created. This list will include the
+				proccess anthony has to go through to navigate my website.</p>
+				<ol>
+					<li>Anthony clicks on the link to my website and the home page loads.</li>
+					<li>Then he procceds to read the brief content outlining the website.</li>
+					<li>Next he clicks on the link "About Me" on the bar closer to the top of the page and it loads.</li>
+					<li>After reading all the information about my history and work experience Anthony proceeds to click on the "My Work/Projects"
+						page.</li>
+					<li>Anthony takes some time to look at the capstone project that I worked on and is interested in contacting me, he clicks on
+					the contact link in the top right and sends me a direct email.</li>
+				</ol>
 		</main>
 	</body>
 </html>
